34267ef1fd2240e9d1a45d79c47eaded39b12c97
mosluce <mosluce@gmail.com>
2014-11-28 10:54:44 +0800

    add user provider and tested

--- controllers/user.js
+++ controllers/user.js
@@ -2,10 +2,22 @@
  * Created by mosluce on 14/11/27.
  */
 
-var express = require('express'), router = express.Router();
+var express = require('express'), app = express.Router();
+var userProvider = require('../libs/user-provider');
 
-router.get('/', function(req, res) {
+app.get('/', function(req, res) {
    res.render('user/index.ejs', {code: '<div>mosluce</div>'});
 });
 
-module.exports = router;
+app.post('/login', function(req, res) {
+   userProvider.authenticate(req.body.username, req.body.password, function(err, result) {
+      if(err) {
+         res.send(err);
+         return;
+      }
+
+      res.json(result);
+   });
+});
+
+module.exports = app;
