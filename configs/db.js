77c432c1faa3e9464c0708809ebe1906a031f8db
mosluce <mosluce@gmail.com>
2014-12-01 10:44:24 +0800

    encrypt

--- configs/db.js
+++ configs/db.js
@@ -7,7 +7,10 @@ module.exports = (function() {
             user: process.env.ACC_MSSQL_USER,
             password: process.env.ACC_MSSQL_PASSWORD,
             server: process.env.ACC_MSSQL_SERVER,
-            database: process.env.ACC_MSSQL_DATABASE
+            database: process.env.ACC_MSSQL_DATABASE,
+            "options": {
+                "encrypt": true
+            }
         };
     } else {
         return require('./db_development.json');
