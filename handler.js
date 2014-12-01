37354a289c408b333d276c2efc595a15c6efa79f
mosluce <mosluce@gmail.com>
2014-12-01 11:29:02 +0800

    加入檢查帳號是否可用

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@
  */
 
 var express = require('express'), app = express(), mssql = require('mssql');
+var dbconfig = require('./configs/db');
 
 function error(res, err) {
     res.json({
@@ -11,9 +12,7 @@ function error(res, err) {
     });
 }
 
-app.post('/login', function(req, res, next) {
-    var dbconfig = require('./configs/db');
-
+app.post('/login', function(req, res) {
     var conn = new mssql.Connection(dbconfig, function(err) {
         if(err) {
             error(res, err);
@@ -45,9 +44,45 @@ app.post('/login', function(req, res, next) {
                 res.json({
                     success: true,
                     data: {
-                        userid: recordset[0].USERNO,
-                        username: recordset[0].USER_NAME
+                        username: recordset[0].USERNO,
+                        name: recordset[0].USER_NAME
                     }
+                });
+
+                ps.unprepare();
+            });
+        });
+    });
+});
+
+app.post('/available', function(req, res) {
+    var conn = new mssql.Connection(dbconfig, function(err) {
+        if(err) {
+            error(res, err);
+            return;
+        }
+
+        var ps = new mssql.PreparedStatement(conn);
+        ps.input('username', mssql.NVarChar);
+        ps.prepare("select count(*) as count from Zuser where USERNO=@username " +
+        "AND APSYSNO='EPURCSYS' " +
+        "AND ZLEVEL!='00'", function(err) {
+            if(err) {
+                error(res, err);
+                return;
+            }
+
+            ps.execute({
+                username: req.body.username
+            }, function(err, recordset) {
+                if(err) {
+                    error(res, err);
+                    return;
+                }
+
+                res.json({
+                    success: true,
+                    data: (recordset[0]['count'] > 0)
                 });
 
                 ps.unprepare();
